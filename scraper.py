810303584821e06cc63ed658c59e6e300206f7cb
Enygma <57713995+Aswin-A-Arun@users.noreply.github.com>
2021-03-31 18:38:56 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,12 +2,12 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
 import csv
-START_URL = "https://exoplanets.nasa.gov/exoplanet-catalog/"
+START_URL = "https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars"
 browser = webdriver.Chrome("/Users/apoorvelous/Downloads/chromedriver")
 browser.get(START_URL)
 time.sleep(10)
 def scrape():
-    headers = ["name", "light_years_from_earth", "planet_mass", "stellar_magnitude", "discovery_date"]
+    headers = ["name", "distance", "mass", "radius"]
     planet_data = []
     for i in range(0, 428):
         soup = BeautifulSoup(browser.page_source, "html.parser")
